3341c019a7663ff88fdc0a418bc3ff83052b71fb
KingNelx <tapiajonel606@gmail.com>
2022-06-05 16:17:59 +0800

    some changes

---
+++ CRUD/CRUD.java
@@ -0,0 +1,8 @@
+package CRUD;
+import java.util.Scanner;
+
+public class CRUD {
+    public static void main (String[]args){
+        Scanner input = new Scanner(System.in);
+    }
+}
---
+++ CRUD/Option.java
@@ -0,0 +1,10 @@
+package CRUD;
+
+public class Option {
+    public void options(){
+        System.out.println(" 1. Add name ");
+        System.out.println(" 2. Display name ");
+        System.out.println(" 3. Update name ");
+        System.out.println(" 4. Delete name ");
+    }
+}
